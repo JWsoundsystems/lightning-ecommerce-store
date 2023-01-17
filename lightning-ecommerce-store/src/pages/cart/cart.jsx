6771a720710f968750aac900699be7980de5cf4a
Jeff Walsh <jwsoundsystems@gmail.com>
2023-01-17 14:00:01 -0800

    added cart functionality & styling

--- lightning-ecommerce-store/src/pages/cart/cart.jsx
+++ lightning-ecommerce-store/src/pages/cart/cart.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { PRODUCTS } from '../../products';
+import { ShopContext } from '../../context/shop-context';
+import { CartItem } from './cart-item';
+import "./cart.css";
 
 export const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
+
   return (
     <div className='cart'>
-        <div className='cartTitle'>
-            <h1> Cart </h1>
+       <div>
+        <h1> Your Cart Items </h1>
+        <div className="cartItems">
+          {PRODUCTS.map((product) => {
+            if (cartItems[product.id] !== 0) {
+              return <CartItem data={product} />;
+            }
+          })}
         </div>
-    </div>
+       </div>
+    </div> 
   )
 }
+ 
