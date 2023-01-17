a792ee35b1436f342182e1eeab782c0d46986aba
Jeff Walsh <jwsoundsystems@gmail.com>
2023-01-17 12:07:32 -0800

    array items and routes added

---
+++ lightning-ecommerce-store/src/pages/cart/cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div className='cart'>
+        <div className='cartTitle'>
+            <h1> Cart </h1>
+        </div>
+    </div>
+  )
+}
---
+++ lightning-ecommerce-store/src/products.js
@@ -0,0 +1,24 @@
+import product1 from './assets/red.jpg';
+import product2 from './assets/green.jpg';
+import product3 from './assets/flakes.jpeg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Red Chile",
+        price: 12.99,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "Green Chile",
+        price: 12.99,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "Chile Flakes",
+        price: 12.99,
+        productImage: product3
+    },
+]
